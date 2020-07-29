3d7f443c4669aa10371380a6d92413115adb4eb6
epirnat <edi.pirnat@gmail.com>
2020-07-29 11:00:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const notes = [ {
+  title: 'Trip ideas',
+  body: 'Go to Bled'
+}, {
+  title: 'Books to read',
+  body: 'Deep Work by Cal Newport'
+}, {
+  title: 'For later',
+  body: 'Look up cool bird facts'
+}];
+
