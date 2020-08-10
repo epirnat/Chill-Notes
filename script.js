ff17b10ad07edaab428dc27937e6bad797b83b2c
epirnat <edi.pirnat@gmail.com>
2020-08-10 12:56:06 +0200

    rewrote note filter function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const filters = {
   searchText: ''
 };
 
+const renderNotes = function (notes, filters) {
+  const filteredNotes = notes.filter(function (note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+
+  document.querySelector('#notes').innerHTML = '';
+
+  filteredNotes.forEach(function (note) {
+    const noteEl = document.createElement('p');
+    noteEl.textContent = note.title;
+    document.querySelector('div').appendChild(noteEl);
+  })
+};
+
+renderNotes(notes, filters);
